151d1e2218aa829cad6ad233a43578210da9eb8b
Marrgon <75068135+Marrgon@users.noreply.github.com>
2023-05-31 19:45:40 +0200

    Dodanie timera

--- Kulki/Dane/BallList.cs
+++ Kulki/Dane/BallList.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Numerics;
+using System.Threading;
+
 
 namespace TPW.Dane
 {
     internal class BallsList
     {
         private readonly List<Ball> ballsList;
+        private Logger logger;
+        private Timer timer;
 
         public List<Ball> GetBallsList()
         {
@@ -15,29 +20,57 @@ namespace TPW.Dane
         public BallsList()
         {
             this.ballsList = new List<Ball>();
+            this.timer = new Timer(UpdateBallsPositions, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
         }
 
         public void AddBalls(int howMany)
         {
+            logger = new Logger();
+            
+
             for (int i = 0; i < howMany; i++)
             {
-                ballsList.Add(new Ball(i + 1));
+                Ball ball = new Ball(i + 1);
+                ball.logger = logger;
+                ballsList.Add(ball);
+                
+
+            }
+            
+        }
+
+        private void UpdateBallsPositions(object state)
+        {
+            foreach (var ball in ballsList)
+            {
+                ball.Simulate();
             }
         }
+
         public Ball GetBall(int id)
         {
-            return ballsList[id - 1];
+            if (id >= 1 && id <= ballsList.Count)
+            {
+                return ballsList[id - 1];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), id, "Invalid ball ID. The provided ID is outside the valid range.");
+            }
         }
+
         public void SetBallSpeed(int id, Vector2 velocity)
         {
-            ballsList[id].Velocity = velocity;
+            if (id >= 1 && id <= ballsList.Count)
+            {
+                ballsList[id - 1].Velocity = velocity;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), id, "Invalid ball ID. The provided ID is outside the valid range.");
+            }
         }
-
-
-
-
-
-
     }
 }
+
 
